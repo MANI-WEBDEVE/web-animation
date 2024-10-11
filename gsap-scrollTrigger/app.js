4c1fc0b830d55274fb07ed596fb7e6157b19d771
Muhammad Inam <nudmaufon@gmail.com>
2024-10-11 23:54:43 +0500

    gsap-scrollTrigger

---
+++ gsap-scrollTrigger/app.js
@@ -0,0 +1,48 @@
+// gsap.to(".box", {
+//   x: 1100,
+//   duration: 2,
+//   delay: 1,
+//   backgroundColor: "red",
+//   borderRadius: "50%",
+//   rotate: 180,
+//   scale: 0.5,
+//   repeat: -1,
+//   yoyo: true,
+// });
+
+// gsap.from("li", {
+//     y:20,
+//     duration: 1,
+//     stagger: 0.2,
+//     opacity: 0,
+//     delay: 1,
+//     // repeat: -1,
+//     // yoyo: true
+// })
+
+// let tl = gsap.timeline({
+//     repeat: -1,
+//     yoyo: true
+// })
+
+// tl.to(".box1", {
+//     x:1100,
+//     duration:3,
+//     backgroundColor: "green",
+//     rotate: 180,
+//     scale: 0.5
+// })
+// tl.to(".box2", {
+//     x:1100,
+//     duration:3,
+//     backgroundColor: "green",
+//     rotate: 180,
+//     scale: 0.5
+// })
+// tl.to(".box3", {
+//     x:1100,
+//     duration:3,
+//     backgroundColor: "green",
+//     rotate: -180,
+//     scale: 0.5
+// })
