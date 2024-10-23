76de7188947a72498283c0cb91ea516609067c3c
Muhammad Inam <nudmaufon@gmail.com>
2024-10-23 17:05:08 +0500

    ✨

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -1,16 +1,30 @@
 import {motion} from 'framer-motion'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <>
-     <motion.div className='box'
-     animate={{x: 900 , rotate: 180}}
-     transition={{duration: 1, delay: 1, ease:"backInOut", repeat: Infinity, velocity: 0.2}}
-     >
+    // <>
+    //  <motion.div className='box'
+    // //  animate={{x: 900 , rotate: 180}}
+    // drag
+    // dragConstraints={{left: 100, right: 100, top:500, bottom: 100}}
+    //  transition={{duration: 1, delay: 1, ease:"backInOut", repeat: Infinity, velocity: 0.2}}
+    //  >
 
-     </motion.div>
+    //  </motion.div>
+    // </>
+    <>
+      <motion.div
+      layout
+      // data-isOpen={isOpen}
+      initial={{ borderRadius: 50 }}
+      className="parent"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <motion.div layout className="child" />
+    </motion.div>
     </>
   )
 }
